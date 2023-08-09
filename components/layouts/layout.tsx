ef6a3c3c937d3f90b2d1a05b343359a0b0caa1a7
박지수 <jisupark2000@gmail.com>
2023-08-09 17:40:45 +0900

    Feat 헤더 Layout

---
+++ components/layouts/layout.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from './header';
+import HeadMeta, { HeadMetaProps } from './headMeta';
+
+interface Props extends HeadMetaProps {
+  children: React.ReactNode;
+}
+
+const Layout: React.FC<Props> = ({ metaTitle, metaDescription, children }) => {
+  return (
+    <div>
+      <HeadMeta {...{ metaTitle, metaDescription }} />
+      <Header />
+      <div className='pt-78'>{children}</div>
+    </div>
+  );
+};
+
+export default Layout;
