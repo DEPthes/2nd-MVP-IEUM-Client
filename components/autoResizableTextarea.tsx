fb696df5899a2ef656645b32a92c8500c6174edf
박지수 <jisupark2000@gmail.com>
2023-08-11 16:46:15 +0900

    Feat AutoResizableTextarea 컴포넌트

---
+++ components/autoResizableTextarea.tsx
@@ -0,0 +1,23 @@
+import { ForwardedRef, TextareaHTMLAttributes } from 'react';
+
+interface Props extends TextareaHTMLAttributes<HTMLTextAreaElement> {
+  textareaRef?: ForwardedRef<HTMLTextAreaElement>; // ref 객체를 전달
+}
+
+// 자동으로 높이가 조절되는 textarea
+// 최소 높이를 지정하려면 min-height 속성 추가
+function AutoResizableTextarea({ textareaRef, children, ...props }: Props) {
+  const handleTextareaChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    // scrollHeight을 구하기 전 줄바꿈 공백을 없애기 위해 높이를 초기화한다
+    event.target.style.height = '0px';
+    const { borderTopWidth, borderBottomWidth } = getComputedStyle(event.target);
+    const { scrollHeight } = event.target;
+    // height 늘리기
+    const newHeight = scrollHeight + parseInt(borderTopWidth) + parseInt(borderBottomWidth);
+    event.target.style.height = `${newHeight}px`;
+  };
+
+  return <textarea ref={textareaRef} onChange={handleTextareaChange} {...props}></textarea>;
+}
+
+export default AutoResizableTextarea;
