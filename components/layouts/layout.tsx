0ef6003204b896af0863e407ab7c18c760c5049d
박지수 <jisupark2000@gmail.com>
2023-08-17 23:52:53 +0900

    Feat 페이지 기본 패딩 Layout.tsx에 적용

--- components/layouts/layout.tsx
+++ components/layouts/layout.tsx
@@ -8,11 +8,13 @@ interface Props extends HeadMetaProps {
 
 const Layout: React.FC<Props> = ({ metaTitle, metaDescription, children }) => {
   return (
-    <div>
+    <>
       <HeadMeta {...{ metaTitle, metaDescription }} />
       <Header />
-      <div className='pt-78'>{children}</div>
-    </div>
+      <div className='pt-78 min-h-screen'>
+        <div className='px-24 py-40 tablet:px-32 tablet:py-56 desktop:px-64 desktop:py-64'>{children}</div>
+      </div>
+    </>
   );
 };
 
