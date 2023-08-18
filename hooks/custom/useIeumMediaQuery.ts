c2363ca547186bb1b4016d9bdecdc86f79960785
박지수 <jisupark2000@gmail.com>
2023-08-18 22:54:33 +0900

    Fix mediaQuery 0.1 단위로 변경

--- hooks/custom/useIeumMediaQuery.ts
+++ hooks/custom/useIeumMediaQuery.ts
@@ -3,8 +3,8 @@ import useMediaQuery from './useMediaQuery';
 // ieum 디자인 체계에 맞는 미디어쿼리
 export default function useIeumMediaQuery() {
   const isDesktop = useMediaQuery({ minWidth: 1136 });
-  const isTablet = useMediaQuery({ minWidth: 600, maxWidth: 1135 });
-  const isMobile = useMediaQuery({ maxWidth: 599 });
+  const isTablet = useMediaQuery({ minWidth: 600, maxWidth: 1135.9 });
+  const isMobile = useMediaQuery({ maxWidth: 599.9 });
 
   return { isDesktop, isTablet, isMobile };
 }
