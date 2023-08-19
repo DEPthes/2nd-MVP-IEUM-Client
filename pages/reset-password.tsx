49d836d549bbe1288be7f6a6ba2112d8d4e96c8b
박지수 <jisupark2000@gmail.com>
2023-08-19 15:06:47 +0900

    Fix 비밀번호 재설정 페이지 초기화면

--- pages/reset-password.tsx
+++ pages/reset-password.tsx
@@ -6,19 +6,19 @@ import Layout from '@/components/layouts/layout';
 import AuthEmail from '@/components/authEmail';
 
 const ResetPassword = () => {
-  const [component, setComponent] = useState<'Auth' | 'Email'>('Email');
+  const [component, setComponent] = useState<'email' | 'password'>('email');
   const [email, setEmail] = useState<string>('');
 
   return (
     <Layout onlyAccess='notUser'>
-      {component === 'Auth' && (
+      {component === 'email' && (
         <AuthEmail
           title={<h1 className='text-primary text-center font-heading--lg mb-24'>비밀번호 재설정</h1>}
-          moveNextPage={() => setComponent('Email')}
+          moveNextPage={() => setComponent('password')}
           setEmail={setEmail}
         />
       )}
-      {component === 'Email' && <PasswordPassword email={email} />}
+      {component === 'password' && <PasswordPassword email={email} />}
     </Layout>
   );
 };
